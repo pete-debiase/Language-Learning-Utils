17903c7a87d037db8f950f2d7d0de6c96602aece
Pete DeBiase <pete@debiase.io>
2022-04-27 04:37:38 -0700

    Cosmetic reorg/rename

--- ext_reading_zh.py
+++ ext_reading_zh.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Process annotated extensive reading text into Anki cards"""
+"""Generate Anki cards from annotated extensive reading text"""
 
 import json
 import re
@@ -13,7 +13,7 @@ SOURCE = '我們是朋友嗎？'
 INPUT_FILE = r'C:\Users\pete\Dropbox\Just Friends.read'
 
 # ┌─────────────────────────────────────────────────────────────────────────────
-# │ Business
+# │ CEDICT Lookup
 # └─────────────────────────────────────────────────────────────────────────────
 filename = r'C:\Users\pete\ALL\Languages\ZH\CEDICT\cedict_ts.json'
 with open(filename, 'r', encoding='utf-8') as f:
@@ -22,60 +22,63 @@ with open(filename, 'r', encoding='utf-8') as f:
 def build_cedict_table(word: str) -> str:
     """Build a nice HTML table for the specified string."""
     try:
+        # Look up in CEDICT
         cedict_entries = cedict[word]
         trad = cedict_entries[0]['t']
         simp = cedict_entries[0]['s']
 
-        word = '<br>'.join(list(dict.fromkeys([trad, simp])))
+        word = '<br>'.join(list(dict.fromkeys([trad, simp]))) # Preserve order
         pinyins_unicode = [entry['p'] for entry in cedict_entries]
         pinyins_numeric = [entry['p#'] for entry in cedict_entries]
         defs = [entry['d'] for entry in cedict_entries]
 
+        # Tag pinyin
         pinyins_tagged = []
         for pu, pn in zip(pinyins_unicode, pinyins_numeric):
-            pinyin_tagged = tag_pinyin(pu, pn)
+            pinyin_tagged = tonetag_pinyin(pu, pn)
             pinyins_tagged.append(pinyin_tagged)
 
+        # Generate/format HTML table
         a = Airium()
         with a.table():
             for p, d in zip(pinyins_tagged, defs):
-                cells = [word, p, d]
+                td_cells = [word, p, d]
                 with a.tr():
-                    [a.td(_t=cell) for cell in cells]
+                    [a.td(_t=cell) for cell in td_cells]
         table = str(a)
-        table = table.replace('\n', '')
-        table = re.sub(r'>\s+<', '><', table)
-        table = table.replace('</span><span', '</span> <span')
+        table = re.sub(r'(\n\s+|\n)', '', table) # Unindent/unwrap
     except KeyError:
         table = 'none'
     return table
 
-def tag_pinyin(pinyin_unicode: str, pinyin_numeric: str) -> str:
+def tonetag_pinyin(pinyin_unicode: str, pinyin_numeric: str) -> str:
     """Span-tag pinyin by tone (for displaying pinyin with color)."""
-    tagvals = []
+    syllables_tagged = []
     syllables_u, syllables_n = pinyin_unicode.split(' '), pinyin_numeric.split(' ')
     for syllable_u, syllable_n in zip(syllables_u, syllables_n):
         tone = re.findall(r'\d', syllable_n)[0]
-        tagval = f'<span class="tone{tone}">{syllable_u}</span>'
-        tagvals.append(tagval)
-    tagged_text = ' '.join(tagvals)
+        syllable_tagged = f'<span class="tone{tone}">{syllable_u}</span>'
+        syllables_tagged.append(syllable_tagged)
+    tagged_text = ' '.join(syllables_tagged)
     return tagged_text
 
-
+# ┌─────────────────────────────────────────────────────────────────────────────
+# │ Process Input Text and Generate Anki TSV
+# └─────────────────────────────────────────────────────────────────────────────
 with open(INPUT_FILE, 'r', encoding='utf-8') as f:
-    text = [line.strip() for line in f]
-text = [_ for _ in text if _ and '{' in _ and '}' in _]
+    sentences = [line.strip() for line in f]
+sentences = [_ for _ in sentences if _ and '{' in _ and '}' in _] # Sentences with {unknown words}
 
 tsv_anki = []
-for sentence in text:
+for sentence in sentences:
     targets = re.findall(r'\{(.*?)\}', sentence)
     for target in targets:
-        sentence2 = sentence.replace(f'{{{target}}}', f'<span class="target">{target}</span>')
-        sentence2 = sentence2.replace('{', '').replace('}', '')
+        sentence = sentence.replace(f'{{{target}}}', f'<span class="target">{target}</span>')
+        sentence = sentence.replace('{', '').replace('}', '')
 
         cedict_table = build_cedict_table(target)
 
-        tsv = f'{sentence2}\t\t{cedict_table}\t\t\t\t{SOURCE}'
+        tsv = f'{sentence}\t\t{cedict_table}\t\t\t\t{SOURCE}'
         tsv_anki.append(tsv)
 
 with open('temp.tsv', 'w+', newline='\n', encoding='utf-8') as f:
