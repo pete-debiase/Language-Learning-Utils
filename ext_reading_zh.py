53b6fe2b97f45654759338a47cdc272f04a64220
Pete DeBiase <pete@debiase.io>
2022-04-27 03:33:29 -0700

    Add support for new CEDICT format + improve presentation of lookup results

--- ext_reading_zh.py
+++ ext_reading_zh.py
@@ -4,6 +4,8 @@
 import json
 import re
 
+from airium import Airium
+
 # ┌─────────────────────────────────────────────────────────────────────────────
 # │ Setup
 # └─────────────────────────────────────────────────────────────────────────────
@@ -17,6 +19,32 @@ filename = r'C:\Users\pete\ALL\Languages\ZH\CEDICT\cedict_ts.json'
 with open(filename, 'r', encoding='utf-8') as f:
     cedict = json.load(f)
 
+def build_cedict_table(word: str) -> str:
+    """Build a nice HTML table for the specified string."""
+    try:
+        cedict_entries = cedict[word]
+        trad = cedict_entries[0]['t']
+        simp = cedict_entries[0]['s']
+
+        word = '<br>'.join(list(dict.fromkeys([trad, simp])))
+        pinyins = [entry['p'] for entry in cedict_entries]
+        defs = [entry['d'] for entry in cedict_entries]
+
+        a = Airium()
+        with a.table():
+            for p, d in zip(pinyins, defs):
+                cells = [word, p, d]
+                with a.tr():
+                    [a.td(_t=cell) for cell in cells]
+
+        table = str(a)
+        table = table.replace('\n', '')
+        table = re.sub(r'>\s+<', '><', table)
+    except KeyError:
+        table = 'none'
+    return table
+
+
 with open(INPUT_FILE, 'r', encoding='utf-8') as f:
     text = [line.strip() for line in f]
 text = [_ for _ in text if _ and '{' in _ and '}' in _]
@@ -27,18 +55,11 @@ for sentence in text:
     for target in targets:
         sentence2 = sentence.replace(f'{{{target}}}', f'<span class="target">{target}</span>')
         sentence2 = sentence2.replace('{', '').replace('}', '')
-        try:
-            trad = cedict[target]['trad']
-            simp = cedict[target]['simp']
-            pinyin = cedict[target]['pinyin']
-            defs = cedict[target]['defs']
-        except KeyError:
-            trad = simp = pinyin = 'none'
-            defs = ''
 
-        tsv = f'{sentence2}\t\t{trad}<br>{simp}<br>{pinyin}\t{defs}\t\t\t{SOURCE}'
+        cedict_table = build_cedict_table(target)
+
+        tsv = f'{sentence2}\t\t{cedict_table}\t\t\t\t{SOURCE}'
         tsv_anki.append(tsv)
-
 
 with open('temp.tsv', 'w+', newline='\n', encoding='utf-8') as f:
     f.write('\n'.join(tsv_anki))
