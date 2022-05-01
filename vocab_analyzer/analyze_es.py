628a9af2db73544a3b4d80c7f166ab3a9207a877
Pete DeBiase <pete@debiase.io>
2022-05-01 03:25:52 -0700

    Implement vocab analyzer for Spanish

---
+++ vocab_analyzer/analyze_es.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""Analyze a new piece of Spanish content"""
+
+from collections import Counter
+from datetime import datetime
+import json
+import re
+
+import spacy
+
+# ┌─────────────────────────────────────────────────────────────────────────────
+# │ Setup
+# └─────────────────────────────────────────────────────────────────────────────
+TITLE = 'Orbiter9'
+INPUT_FILE = r'C:\Users\pete\ALL\Languages\ES\_fulltexts\Orbiter9.txt'
+CONTENT_TYPE = 'movie'
+
+NONLEMMAS = {'.', '[', ']', ',', '?', '¿', '-', '¡', '!', '…', '“', '”'}
+
+# ┌─────────────────────────────────────────────────────────────────────────────
+# │ Lemma-Based Analysis
+# └─────────────────────────────────────────────────────────────────────────────
+# Load up necessary files
+with open(INPUT_FILE, 'r', encoding='utf-8') as f:
+    fulltext = f.read().lower()
+    fulltext = re.sub(r'( |\.|,)\d+( |\.|,)', ' ', fulltext) # Discard stand-alone numbers.
+    fulltext = fulltext.split('\n')
+
+with open('seen_lemmas_es.json', 'r', encoding='utf-8') as f:
+    seen_lemmas = Counter(json.load(f))
+
+# Total lemmas
+nlp = spacy.load('es_dep_news_trf')
+
+lemmas = []
+for line in fulltext:
+    tokens = nlp(line)
+    lemmas.extend([t.lemma_ for t in tokens])
+lemmas = [l for l in lemmas if l and l not in NONLEMMAS]
+
+lemmas_new = [l for l in lemmas if l not in seen_lemmas]
+
+# Unique lemmas
+lemmas_unique = Counter(lemmas)
+lemmas_new_unique = Counter(lemmas_new)
+
+# Display stats
+print(f'total_lemmas: {len(lemmas)}')
+print(f'new_lemmas: {len(lemmas_new_unique)}')
+print(f'unique_lemmas: {len(lemmas_unique)}')
+
+# ┌─────────────────────────────────────────────────────────────────────────────
+# │ Create Vocab Candidate List
+# └─────────────────────────────────────────────────────────────────────────────
+# TODO later...
+
+# ┌─────────────────────────────────────────────────────────────────────────────
+# │ Update Records
+# └─────────────────────────────────────────────────────────────────────────────
+# Update seen characters and lemmas
+seen_lemmas += lemmas_unique
+seen_lemmas = dict(seen_lemmas.most_common()) # Keep sorted by freq
+
+with open('seen_lemmas_es.json', 'w+', newline='\n', encoding='utf-8') as f:
+    json.dump(seen_lemmas, f, indent=2, ensure_ascii=False)
+
+# Update seen content records
+with open('seen_content_es.json', 'r', encoding='utf-8') as f:
+    seen_content = json.load(f)
+
+content_summary = {'time': f'{datetime.now():%Y-%m-%d %H:%M:%S}',
+                   'type': CONTENT_TYPE,
+                   '#l': len(lemmas),
+                   '%ln': round(len(lemmas_new) / len(lemmas), 2),
+                   '#ln': len(lemmas_new_unique),
+                   '#lq': len(lemmas_unique),
+                   'ln': '|'.join(lemmas_new_unique.keys()),
+                   'lq': '|'.join(lemmas_unique.keys()),
+                   }
+seen_content[TITLE] = content_summary
+
+with open('seen_content_es.json', 'w+', newline='\n', encoding='utf-8') as f:
+    json.dump(seen_content, f, indent=2, ensure_ascii=False)
