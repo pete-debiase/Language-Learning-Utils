426fe2dbde816d9ebe9c7c4941ecd9080f722a04
Pete DeBiase <pete@debiase.io>
2022-06-07 06:42:07 -0700

    Simplify/rename `glue.py`

--- srt/glue.py
+++ srt/glue.py
@@ -1,39 +1,17 @@
 #!/usr/bin/env python3
 """Glue together subtitles to facilitate study in Anki (2018/01/10)"""
 
-from statistics import mean
 import os
+from statistics import mean
 
 import pysrt
-import webvtt
 
 # ┌─────────────────────────────────────────────────────────────────────────────
 # │ Setup
 # └─────────────────────────────────────────────────────────────────────────────
-root_vtt = r'C:\Users\pete\ALL\Languages\ES\Orbiter 9\subs\vtt/'
-root_srt = r'C:\Users\pete\ALL\Languages\ES\Orbiter 9\subs\srt/'
-TARGET_DURATION_MS = 15000
-
-# ┌─────────────────────────────────────────────────────────────────────────────
-# │ Convert VTT to SRT
-# └─────────────────────────────────────────────────────────────────────────────
-vtt_files = os.listdir(root_vtt)
-for filename in vtt_files:
-    vtt = webvtt.read(root_vtt + filename)
-    output_filename = root_srt + filename.replace('.vtt', '.srt')
-    with open(output_filename, 'w+', newline='\n', encoding='utf-8') as f:
-        vtt.write(f, format='srt')
-
-# ┌─────────────────────────────────────────────────────────────────────────────
-# │ Sanitize SRT
-# └─────────────────────────────────────────────────────────────────────────────
-srt_files = [f for f in os.listdir(root_srt) if 'glued' not in f]
-
-for filename in srt_files:
-    srt = pysrt.open(root_srt + filename)
-    for sub in srt:
-        sub.text = sub.text_without_tags.replace('\n', ' ')
-    srt.save(root_srt + filename, encoding='utf-8')
+root_srt = r'C:\~\Languages\ES\SUBS2SRS\Contratiempo\subs\srt/'
+root_glued = r'C:\~\Languages\ES\SUBS2SRS\Contratiempo\subs\glued/'
+TARGET_DURATION_MS = 13500
 
 # ┌─────────────────────────────────────────────────────────────────────────────
 # │ Glue SRT
@@ -78,5 +56,5 @@ for filename in srt_files:
 
     # Output glued files
     srt.data = subs_glued
-    output_filename = root_srt + 'glued/' + filename
+    output_filename = root_glued + filename
     srt.save(output_filename, encoding='utf-8')
